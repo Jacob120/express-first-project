e56163fb1aee96b221efc71968ec08f885662272
Jakub Kozik <jakubkozik120@gmail.com>
2022-07-05 20:35:05 +0200

    Fix importing files

--- server.js
+++ server.js
@@ -4,24 +4,37 @@ import path from 'path';
 const app = express();
 const __dirname = path.resolve();
 
+app.use(express.static(path.join(__dirname, '/public')));
+
+app.use((req, res, next) => {
+  res.show = (name) => {
+    res.sendFile(path.join(__dirname, `/views/${name}`));
+  };
+  next();
+});
+
 app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, '/views/index.html'));
+  res.show('index.html');
 });
 
 app.get('/about', (req, res) => {
-  res.sendFile(path.join(__dirname, '/views/about.html'));
+  res.show('about.html');
 });
 
 app.get('/contact', (req, res) => {
-  res.sendFile(path.join(__dirname, '/views/contact.html'));
+  res.show('contact.html');
 });
 
 app.get('/info', (req, res) => {
-  res.sendFile(path.join(__dirname, '/views/info.html'));
+  res.show('info.html');
 });
 
 app.get('/history', (req, res) => {
-  res.sendFile(path.join(__dirname, '/views/history.html'));
+  res.show('history.html');
+});
+
+app.use((req, res) => {
+  res.status(404).send('404 not found...');
 });
 
 app.listen(8000, () => {
