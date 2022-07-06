050336b869e83c4c56708fef371a9f38eb1e88ba
Jakub Kozik <jakubkozik120@gmail.com>
2022-07-06 20:02:31 +0200

    Add handlebars

--- server.js
+++ server.js
@@ -1,36 +1,37 @@
 import express from 'express';
 import path from 'path';
+import { engine } from 'express-handlebars';
 
 const app = express();
 const __dirname = path.resolve();
 
 app.use(express.static(path.join(__dirname, '/public')));
 
-app.use((req, res, next) => {
-  res.show = (name) => {
-    res.sendFile(path.join(__dirname, `/views/${name}`));
-  };
-  next();
-});
+app.engine('.hbs', engine({ extname: '.hbs' }));
+app.set('view engine', '.hbs');
 
 app.get('/', (req, res) => {
-  res.show('index.html');
+  res.render('index', { layout: false });
+});
+
+app.get('/hello/:name', (req, res) => {
+  res.render('Hello', { layout: false, name: req.params.name });
 });
 
 app.get('/about', (req, res) => {
-  res.show('about.html');
+  res.render('about', { layout: false });
 });
 
 app.get('/contact', (req, res) => {
-  res.show('contact.html');
+  res.render('contact', { layout: false });
 });
 
 app.get('/info', (req, res) => {
-  res.show('info.html');
+  res.render('info', { layout: false });
 });
 
 app.get('/history', (req, res) => {
-  res.show('history.html');
+  res.render('history', { layout: false });
 });
 
 app.use((req, res) => {
